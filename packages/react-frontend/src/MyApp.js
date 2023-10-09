b0a523a3603a0b3f22e456d491e839e41f216b29
Joshua Smith <joshuasmith6556@gmail.com>
2023-10-08 17:18:24 -0700

    Finish guided lab

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -26,12 +26,32 @@ function MyApp() {
   useEffect(() => {
     fetchUsers()
       .then(res => res.json())
-      .then(json => setCharacters(json["users_list"]))
+      .then(json => setCharacters(json['users_list']))
       .catch(error => console.log(error))
   }, [])
 
+  function postUser(person) {
+    const promise = fetch('http://localhost:8000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(person),
+    })
+
+    return promise
+  }
+
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+
   return (
-    <div className="container">
+    <div className='container'>
       <Table characterData={characters} 
         removeCharacter={removeOneCharacter} />
       <Form handleSubmit={updateList} />
